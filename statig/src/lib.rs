9c77c53f0f127e5d68e29b7bf763e69229dab09b
Maxim Deloof <20443795+mdeloof@users.noreply.github.com>
2024-04-20 22:12:45 +0200

    Add support for `async` on `no_std`

--- statig/src/lib.rs
+++ statig/src/lib.rs
@@ -815,6 +815,9 @@ pub use statig_macro::action;
 
 /// Prelude containing the necessary imports for use with macro.
 pub mod prelude {
+    #![allow(ambiguous_glob_reexports)]
+    #![allow(unused_imports)]
+
     #[cfg(feature = "async")]
     pub use crate::awaitable::{IntoStateMachineExt as _, StateExt as _, *};
     pub use crate::blocking::{IntoStateMachineExt as _, StateExt as _, *};
